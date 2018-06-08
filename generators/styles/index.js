d1bc58f0b6850fd1aa86a2b76d52ca99fe855267
Euirim Choi <euirim@me.com>
2018-06-08 03:32:22 -0500

    Replace old bootstrap 3 with bootstrap 4

--- generators/styles/index.js
+++ generators/styles/index.js
@@ -3,12 +3,15 @@ const Generator = require('yeoman-generator');
 module.exports = class extends Generator {
   writing() {
     this.fs.copy(
+      this.templatePath('src/scss/_bootstrap.scss'),
+      this.destinationPath('src/scss/_bootstrap.scss'));
+    this.fs.copy(
       this.templatePath('src/scss/main.scss'),
       this.destinationPath('src/scss/main.scss'));
   }
   install() {
     const dependencies = [
-      'bootstrap-sass'
+      'bootstrap'
     ];
     this.yarnInstall(dependencies, { save: true });
   }
