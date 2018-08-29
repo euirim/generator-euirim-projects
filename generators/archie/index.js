7e294b6720b90d2479e49777e099fcd6570cedc9
Euirim Choi <euirim@me.com>
2018-08-28 21:35:39 -0700

    Add more extensive initial text

--- generators/archie/index.js
+++ generators/archie/index.js
@@ -25,9 +25,14 @@ module.exports = class extends Generator {
       headline: 'Test Maroon Headline',
       subhead: 'This is a sample subhead. Make it yours!',
       sections: [{
-        type: 'text',
-        text: 'Hello, world! This is your content.',
-      }],
+          type: 'text',
+          text: 'Fraternity parties have no place at the University of Chicago. They are horrific, nauseating, and claustrophobic, rife with social anxieties and sweaty bodies weirdly undulating to suggestive music. They are a poor excuse for genuine camaraderie. Instead, through cheap liquor and obnoxiously loud music, they render their guests invincibly stupid and permanently deaf. I avoid them all at costs, particularly because of the music—which I can safely assume would be a sample of the latest noisome albums. But why am I right? For that, we must turn to a philosopher named Hannah Arendt.',
+        },
+        {
+          type: 'text',
+          text: 'Few political theorists of the 20th century were as ingenious, intuitive, and still sorely underappreciated as Hannah Arendt. Her almost three-decade-long enterprise in American academia (including teaching at the University of Chicago) produced a body of literature that masterfully schematized authoritarian regimes, critiqued the liberal nation-state, and offered a new model of politics that emulated the Athenian polis and its veneration of deliberative democracy and a robust public sphere.',
+        }
+      ],
     });
   }
 
@@ -36,6 +41,8 @@ module.exports = class extends Generator {
       'archieml-pipe'
     ];
 
-    this.yarnInstall(dependencies, { save: true });
+    this.yarnInstall(dependencies, {
+      save: true
+    });
   }
 };
