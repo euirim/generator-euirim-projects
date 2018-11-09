28daa64c1530f27dc80944124c525fe6d1624b87
euirim <euirim@me.com>
2018-11-09 02:29:31 -0600

    Lock dependencies

--- generators/bundler-webpack/templates/webpack-dev.config.js
+++ generators/bundler-webpack/templates/webpack-dev.config.js
@@ -20,31 +20,26 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
+        test: /\.(js|jsx)$/,
         use: {
           loader: 'babel-loader',
           options: {
             presets: [
-              [
-                "env", 
-                {
-                  "targets": {
-                    "browsers": ["last 2 versions"]
-                  },
-                  "debug": true,
-                  "modules": false,
-                }
-              ],
-              "react",
-              "stage-0",
-              "airbnb",
-            ]
-          }
+              ['@babel/preset-env', {
+                targets: {
+                  browsers: 'last 2 versions',
+                },
+              }],
+              '@babel/preset-react',
+            ],
+            plugins: [
+              '@babel/proposal-class-properties',
+            ],
+          },
         },
       },
       {
-        test: /\.scss$/,
+        test: /\.s*css$/,
         use: [
           {
             loader: 'style-loader',
